9f221b85894e57f928ddde13bd75cb23c56b4aff
Drewry Pope <dezren39@users.noreply.github.com>
2017-02-16 16:40:05 -0600

    Add files via upload

---
+++ DB_CodeChallenge_1/DB_CodeChallenge_1/Character.vb
@@ -0,0 +1,31 @@
+﻿Public Class Character
+
+    Private cName As String
+    Private cRace As String
+
+
+    Public Property Name() As String
+        Get
+            Return cName
+        End Get
+        Set(ByVal value As String)
+            cName = value
+        End Set
+    End Property
+
+    Public Property Race() As String
+        Get
+            Return cRace
+        End Get
+        Set(ByVal value As String)
+            cRace = value
+        End Set
+    End Property
+
+    Public ReadOnly Property DisplayName() As String
+        Get
+            Dim cDisplayName As String = cName + " - " + cRace
+            Return cDisplayName
+        End Get
+    End Property
+End Class
